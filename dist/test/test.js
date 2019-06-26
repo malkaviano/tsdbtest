9cf99462f1cd10723f6a042108ae247097a0b582
malkaviano <rafael.kss@gmail.com>
2019-06-26 11:06:06 -0300

    first commit

---
+++ dist/test/test.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chai_1 = require("chai");
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            chai_1.expect([1, 2, 3].indexOf(4)).to.equal(-1);
+        });
+    });
+});
+//# sourceMappingURL=test.js.map
